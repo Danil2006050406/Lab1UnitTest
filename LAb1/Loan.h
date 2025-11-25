f528f870b0cfcefa7105092f80b1f1c7c4061167
Danyl_Sakhno <66576537+Danil2006050406@users.noreply.github.com>
2025-11-25 18:06:11 +0200

    Update Loan.h

--- LAb1/Loan.h
+++ LAb1/Loan.h
@@ -1,22 +1,49 @@
-﻿#pragma once
+#pragma once
+#include "Book.h"
 #include <string>
-#include <ctime>
+#include <optional>
 
+/**
+ * @brief Запис про видачу книги читачу.
+ *
+ * Містить інформацію про книгу, читача та статус повернення.
+ */
 class Loan {
-private:
-    std::string loanId, bookIsbn, userId;
-    time_t loanDate, dueDate;
-    bool returned;
 public:
-    Loan(const std::string& lid, const std::string& isbn, const std::string& uid,
-        time_t loanD, time_t dueD);
+    /**
+     * @brief Створює новий запис про видачу.
+     * @param book Книга, яку видають.
+     * @param readerId Ідентифікатор читача.
+     */
+    Loan(const Book& book, const std::string& readerId);
 
-    void markReturned();
+    /**
+     * @brief Позначає, що книга повернена.
+     *
+     * @throws std::logic_error Якщо книга вже була повернена раніше.
+     */
+    void returnBook();
+
+    /**
+     * @brief Перевіряє, чи книга вже повернена.
+     * @return true, якщо книга повернена, false інакше.
+     */
     bool isReturned() const;
-    bool isOverdue() const;
-    int daysLeft() const;
 
-    const std::string& getISBN() const;
-    const std::string& getUserId() const;
-    const std::string& getLoanId() const;
+    /**
+     * @brief Повертає ідентифікатор читача.
+     * @return Рядок із ID читача.
+     */
+    std::string getReaderId() const;
+
+    /**
+     * @brief Повертає видану книгу.
+     * @return Константне посилання на книгу.
+     */
+    const Book& getBook() const;
+
+private:
+    Book book_;
+    std::string readerId_;
+    bool returned_ = false;
 };
